695113dec14bfb4ac1550db38544b30f0a35ca68
l4ndl0s1 <laurusedel@gmail.com>
2025-02-18 11:55:07 +0100

    first commt

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+// tailwind.config.js
+module.exports = {
+  content: [
+    "./*.html",
+    "./src/**/*.{html,js}",
+    // if your final HTML is in another folder, add that too:
+    // "./public/**/*.html"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
